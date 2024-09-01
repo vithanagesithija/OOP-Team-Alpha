8664a0ca2d75aa19874cdd8d3755cde34969bd1d
Malith-Rukshan <singledeveloper.lk@gmail.com>
2024-09-02 00:10:44 +0530

    --

--- src/main/java/com/techrepair/Main.java
+++ src/main/java/com/techrepair/Main.java
@@ -9,6 +9,8 @@ public class Main {
             JFrame frame = new JFrame("TechRepair Management System");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(800, 600);
+
+
             frame.setVisible(true);
         });
     }
