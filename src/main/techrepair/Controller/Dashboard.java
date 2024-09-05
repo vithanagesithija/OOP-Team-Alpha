d653e6e2da7dfd3bcee99a9b0051e7b13868c254
Thejan Go <66713570+thejan64go@users.noreply.github.com>
2024-09-05 19:51:36 +0530

    fix box sizes and names

--- src/main/techrepair/Controller/Dashboard.java
+++ src/main/techrepair/Controller/Dashboard.java
@@ -64,54 +64,54 @@ public class Dashboard extends JFrame {
     public void viewCustomer(){
         CustomerUI customerUI = new CustomerUI();
         customerUI.setContentPane(customerUI.backPane);
-        customerUI.setTitle("Login TechRepair");
-        customerUI.setSize(400,400);
+        customerUI.setTitle("View Customers");
+        customerUI.setSize(1000,800);
         customerUI.setVisible(true);
     }
 
     public void viewEmployee(){
         EmployeeUI employeeUI = new EmployeeUI();
         employeeUI.setContentPane(employeeUI.backPane);
-        employeeUI.setTitle("Login TechRepair");
-        employeeUI.setSize(400,400);
+        employeeUI.setTitle("View Employee");
+        employeeUI.setSize(1000,800);
         employeeUI.setVisible(true);
     }
     public void viewSupplires(){
         SuppliresUI suppliresUI = new SuppliresUI();
         suppliresUI.setContentPane(suppliresUI.backPane);
-        suppliresUI.setTitle("Login TechRepair");
-        suppliresUI.setSize(400,400);
+        suppliresUI.setTitle("View Suppliers");
+        suppliresUI.setSize(1000,800);
         suppliresUI.setVisible(true);
     }
      public void viewInventory(){
          InventoryView inventoryView = new InventoryView();
          inventoryView.setContentPane(inventoryView.backPane);
-         inventoryView.setTitle("Login TechRepair");
-         inventoryView.setSize(400,400);
+         inventoryView.setTitle("View Inventory");
+         inventoryView.setSize(1000,800);
          inventoryView.setVisible(true);
      }
 
      public void viewCustomerJobs(){
          CustomerJobs customerJobs = new CustomerJobs();
          customerJobs.setContentPane(customerJobs.backPane);
-         customerJobs.setTitle("Login TechRepair");
-         customerJobs.setSize(400,400);
+         customerJobs.setTitle("View Customer Jobs");
+         customerJobs.setSize(1000,800);
          customerJobs.setVisible(true);
      }
 
      public void Gview(){
          RepoartGenarate repoartGenarate = new RepoartGenarate();
          repoartGenarate.setContentPane(repoartGenarate.BackPane);
-         repoartGenarate.setTitle("Login TechRepair");
-         repoartGenarate.setSize(400,400);
+         repoartGenarate.setTitle("Report Generate");
+         repoartGenarate.setSize(800,600);
          repoartGenarate.setVisible(true);
      }
 
      public void viewEmail(){
          emailSend emailSend = new emailSend();
          emailSend.setContentPane(emailSend.backPane);
-         emailSend.setTitle("Login TechRepair");
-         emailSend.setSize(400,400);
+         emailSend.setTitle("View Email");
+         emailSend.setSize(1000,800);
          emailSend.setVisible(true);
      }
     public void initializeComponents() {
